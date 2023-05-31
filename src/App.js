e11123f5a0e7127b80b5c715da859b20941b4e02
Matthew Catellier <mc@Matthews-MacBook-Pro.local>
2023-05-30 17:36:29 -0700

    Add validation submit todos using react-hook-forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
 import { TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Container } from '@mui/material';
 import Delete from '@mui/icons-material/Delete';
+import { useForm, Controller } from 'react-hook-form';
 
 function App() {
-  const [task, setTask] = useState({title: '', date: '', priority: ''});
+  const { handleSubmit, control, reset } = useForm();
   const [tasks, setTasks] = useState([]);
 
-  const handleChange = e => {
-    const {name, value} = e.target;
-    setTask(prevTask => ({
-      ...prevTask,
-      [name]: value
-    }));
-  };
-
-  const addTask = e => {
-    e.preventDefault();
-    setTasks([...tasks, task]);
-    setTask({title: '', date: '', priority: ''});
+  const onSubmit = data => {
+    setTasks([...tasks, data]);
+    reset();
   };
 
   const removeTask = index => {
@@ -30,34 +22,52 @@ function App() {
 
   return (
     <Container maxWidth="sm">
-      <form onSubmit={addTask}>
-        <TextField 
-          name="title" 
-          value={task.title} 
-          onChange={handleChange} 
-          placeholder="Task" 
-          required 
-          fullWidth 
-          style={{ margin: "10px 0" }}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="title"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => 
+            <TextField 
+              {...field}
+              placeholder="Task" 
+              required 
+              fullWidth 
+              style={{ margin: "10px 0" }}
+            />
+          }
         />
-        <TextField 
-          type="date" 
-          name="date" 
-          value={task.date} 
-          onChange={handleChange} 
-          required 
-          fullWidth 
-          style={{ margin: "10px 0" }}
+        <Controller
+          name="date"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => 
+            <TextField 
+              {...field}
+              type="date"
+              required 
+              fullWidth 
+              style={{ margin: "10px 0" }}
+            />
+          }
         />
-        <TextField 
-          type="number" 
-          name="priority" 
-          value={task.priority} 
-          onChange={handleChange} 
-          placeholder="Priority" 
-          required 
-          fullWidth 
-          style={{ margin: "10px 0" }}
+        <Controller
+          name="priority"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => 
+            <TextField 
+              {...field}
+              type="number"
+              placeholder="Priority" 
+              required 
+              fullWidth 
+              style={{ margin: "10px 0" }}
+            />
+          }
         />
         <Button variant="contained" color="primary" type="submit">Add Task</Button>
       </form>
@@ -81,4 +91,3 @@ function App() {
 }
 
 export default App;
-
